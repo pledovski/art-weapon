8d54dc2c2282e6eeea4c96c3c8c936e9fcdd778f
pledovski <human.pled@gmail.com>
2023-02-13 00:06:38 +0200

    visits logger implemented

--- artweapon.js
+++ artweapon.js
@@ -13,6 +13,9 @@ const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 
+const geoip = require("geoip-lite");
+const fs = require("fs");
+
 // Load env vars
 // dotenv.config({ path: "./config/config.env" });
 
@@ -72,7 +75,44 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 app.get("/", (req, res) => {
-  res.redirect("https://ra.co/events/1645227");
+  fs.readFile("./_data/visits.json", "utf-8", (err, data) => {
+    let visits = [];
+    const ip = req.socket.remoteAddress;
+    const location = geoip.lookup(ip);
+    if (err) {
+      visits.push({
+        visits: 1,
+        IP: ip,
+        location,
+        userAgent: req.headers["user-agent"],
+        url: req.url,
+        query: req.query,
+        headers: req.headers,
+      });
+    } else {
+      visits = JSON.parse(data);
+      visits.push({
+        visits: visits.length + 1,
+        IP: ip,
+        location,
+        userAgent: req.headers["user-agent"],
+        url: req.url,
+        query: req.query,
+        headers: req.headers,
+      });
+    }
+
+    fs.writeFile("./_data/visits.json", JSON.stringify(visits), (err) => {
+      if (err) {
+        // console.error(err);
+        // res.send("An error occurred.");
+        res.redirect("https://ra.co/events/1645227");
+      } else {
+        // res.send(`Number of visits: ${visits.length}`);
+        res.redirect("https://ra.co/events/1645227");
+      }
+    });
+  });
 });
 
 // Mount routers
